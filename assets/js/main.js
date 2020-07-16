f2c2610db590044ab0a8cf7c0b1e96b087b2e650
alexhlv <alexhlv@gmail.com>
2020-07-16 19:01:56 +0300

    Both tasks are done

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+/* Завдання 1:*/
+
+// Задание 1:
+
+// Реализуйте функцию-конструктор Worker (Работник), который будет иметь следующие свойства: name (имя), surname (фамилия), rate (ставка за день работы), days (количество отработанных дней). Также класс должен иметь метод getSalary(), который будет выводить зарплату работника. Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days. Вот так должен работать наш класс:
+
+function Worker(name, surname, rate, days) {
+    this.name = name;
+    this.surname = surname;
+    this.rate = rate;
+    this.days = days;
+    this.getSalary = function(rate, days) {
+        return this.rate * this.days; 
+    }
+}
+
+let worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.name); //выведет 'Иван'
+console.log(worker.surname); //выведет 'Иванов'
+console.log(worker.rate); //выведет 10
+console.log(worker.days); //выведет 31
+console.log(worker.getSalary()); //выведет 310 - то есть 10*31  
+
+/* Завдання 1:*/
+/* Завдання 2:*/
+
+// Задача 2:
+
+// Создайте функцию-конструктор Calculator (калькулятор) с тремя методами:
+
+// read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+// sum() (суммировать) возвращает сумму сохранённых значений.
+// mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+function Calculator() {
+
+    this.read = function() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    };
+  
+    this.sum = function() {
+      return this.a + this.b;
+    };
+  
+    this.mul = function() {
+      return this.a * this.b;
+    };
+  }
+  
+  let calculator = new Calculator();
+  calculator.read();
+
+  console.log( "Sum=" + calculator.sum() );
+  console.log( "Mul=" + calculator.mul() );
+
+/* Завдання 2:*/
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Вивчення JavaScript
+**Об'єкти**
+
+**Objects**
+
+**Тестові задачі з вивчення об'єктів на [GitHub](https://alexhlv.github.io/Methods-of-objects/)**
+
+**Тестові задачі з вивчення об'єктів на [Сodepen.io](https://codepen.io/hlv/pen/xxZyXga)**
